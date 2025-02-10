df3370fcaa0cc42f287a8f46b1251e144e86ec75
yagoalonso1 <yagoalonso3105@gmail.com>
2025-02-10 19:52:37 +0100

    modify platform model

--- app/Models/Platform.php
+++ app/Models/Platform.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Platform extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name', 'owner'];
+
+    public function apartments(): BelongsToMany
+    {
+        return $this->belongsToMany(Apartment::class, 'platform_apartment')
+                    ->withPivot('register_date', 'premium')
+                    ->withTimestamps();
+    }
 }
