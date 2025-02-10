06b306f1aec2270e58a48128da4bab8777099eea
yagoalonso1 <yagoalonso3105@gmail.com>
2025-02-10 20:34:04 +0100

    modifu routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,4 @@ use App\Http\Controllers\ApartmentController;
 Route::post('/signin', [AuthController::class, 'signin']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::apiResource('/apartments', ApartmentController::class)->only(['index']);
+Route::apiResource('/apartments', ApartmentController::class)->only(['index', 'show']);
