f006f74c1fc8b4dfbb1695d037ee4ac9fa137f1a
yagoalonso1 <yagoalonso3105@gmail.com>
2025-02-10 19:55:39 +0100

    add seeders

--- database/factories/PlatformApartmentFactory.php
+++ database/factories/PlatformApartmentFactory.php
@@ -1,23 +1,22 @@
 <?php
 
 namespace Database\Factories;
-
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\PlatformApartment>
  */
 class PlatformApartmentFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            //
+            'register_date' => fake()->date(),
+            'premium' => fake()->boolean(),
+            'apartment_id' => Apartment::factory(),
+            'platform_id' => Platform::factory(),
         ];
     }
 }
