8deb56e7d3d81f6167c880ba1f4975cce8697179
yagoalonso1 <yagoalonso3105@gmail.com>
2025-02-10 19:52:58 +0100

    modify aprartment model

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -1,10 +1,25 @@
 <?php
 
-namespace App\Models;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Apartment extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['address', 'city', 'postal_code', 'rented_price', 'rented', 'user_id'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function platforms(): BelongsToMany
+    {
+        return $this->belongsToMany(Platform::class, 'platform_apartment')
+                    ->withPivot('register_date', 'premium')
+                    ->withTimestamps();
+    }
 }
