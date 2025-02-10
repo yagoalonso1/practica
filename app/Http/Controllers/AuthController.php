9af895de2fcb0431291001c5449314d12b58968c
yagoalonso1 <yagoalonso3105@gmail.com>
2025-02-10 20:19:26 +0100

    authcontroller created

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,56 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AuthController extends Controller
 {
-    //
+    public function signin(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => ['required', 'string', 'regex:/^[\pL\s]+$/u'], // Solo letras y espacios
+            'email' => ['required', 'string', 'email', 'unique:users'],
+            'password' => ['required', 'string', Password::min(5), 'confirmed'],
+        ]);
+
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Usuario registrado correctamente',
+            'token' => $token,
+            'user' => $user,
+        ], 201);
+    }
+
+
+    public function login(Request $request)
+    {
+        $validated = $request->validate([
+            'email' => ['required', 'string', 'email'],
+            'password' => ['required', 'string', Password::min(5)],
+        ]);
+
+        $user = User::where('email', $validated['email'])->first();
+
+        if (!$user || !Hash::check($validated['password'], $user->password)) {
+            return response()->json(['message' => 'Credenciales incorrectas'], 401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Inicio de sesión exitoso',
+            'token' => $token,
+            'user' => $user,
+        ]);
+    }
 }
