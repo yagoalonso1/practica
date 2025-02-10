9c99663d772c2580fad2a89d16d96a7f88027227
yagoalonso1 <yagoalonso3105@gmail.com>
2025-02-10 20:18:56 +0100

    edit user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory,HasApiTokens, Notifiable;
 
     protected $fillable = ['name', 'email', 'password'];
 
