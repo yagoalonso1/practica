57fd461185896f4fe5af970a08e567e958cf0463
yagoalonso1 <yagoalonso3105@gmail.com>
2025-02-10 20:20:19 +0100

    add roapi.php

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::post('/signin', [AuthController::class, 'signin']);
+Route::post('/login', [AuthController::class, 'login']);
