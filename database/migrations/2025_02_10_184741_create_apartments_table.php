757882926c49fdd14b192908f2dc18cd9745233c
yagoalonso1 <yagoalonso3105@gmail.com>
2025-02-10 19:50:09 +0100

    modify migrations

--- database/migrations/2025_02_10_184741_create_apartments_table.php
+++ database/migrations/2025_02_10_184741_create_apartments_table.php
@@ -4,24 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
-        Schema::create('apartments', function (Blueprint $table) {
+        Schema::create('platforms', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('owner');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::dropIfExists('apartments');
+        Schema::dropIfExists('platforms');
     }
 };
