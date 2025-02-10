88a46820309f66d5de33bb115dcb0abe1f6177d0
yagoalonso1 <yagoalonso3105@gmail.com>
2025-02-10 19:54:14 +0100

    modify platformfactory

--- database/factories/PlatformFactory.php
+++ database/factories/PlatformFactory.php
@@ -9,15 +9,11 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class PlatformFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            //
+            'name' => fake()->company(),
+            'owner' => fake()->name(),
         ];
     }
 }
