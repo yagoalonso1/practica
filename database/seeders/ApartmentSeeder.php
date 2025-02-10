5d5cc6ffd27181beb57d18e41e6f9152a99f2f8c
yagoalonso1 <yagoalonso3105@gmail.com>
2025-02-10 19:57:46 +0100

    add apartment seeder

--- database/seeders/ApartmentSeeder.php
+++ database/seeders/ApartmentSeeder.php
@@ -4,14 +4,11 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+\App\Models\Apartment;
 class ApartmentSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+       Apartment::factory(20)->create();
     }
 }
